afe85748f42be849060388825b0113a78f815b95
Kartik Bhatia <37398223+KnightKnight27@users.noreply.github.com>
2020-05-02 03:34:47 +0530

    Create MinimumPathsum.cpp

---
+++ MinimumPathsum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& dp) 
+    {
+        for(int i=0;i<dp.size();i++)
+        {
+            for(int j=0;j<dp[0].size();j++)
+            {
+                if(i==0 and j==0)
+                {
+                    continue;
+                }
+                if(i==0)
+                {
+                    dp[i][j]+=dp[i][j-1];
+                }
+                else if(j==0)
+                {
+                    dp[i][j]+=dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j]+=min(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[dp.size()-1][dp[0].size()-1];
+    }
+}
